2bd1f4142eaf12005b5a037a09209a64171e3acf
Asker <kotsevasker0@gmail.com>
2023-05-07 18:33:12 +0300

    Initial Commit

---
+++ Tevos-App/Tevos_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tevos_AppApp.swift
+//  Tevos-App
+//
+//  Created by ASKER on 07.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Tevos_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
